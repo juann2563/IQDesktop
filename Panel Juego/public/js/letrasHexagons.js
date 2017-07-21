9b82d7180c00e1ead475d30bf5b5b778932f5ad1
juann2563 <juanzabala2193@gmail.com>
2017-07-20 20:07:17 -0500

    Generato random
    Generated random letters in panel Juego

--- Panel Juego/public/js/letrasHexagons.js
+++ Panel Juego/public/js/letrasHexagons.js
@@ -1,22 +1,16 @@
 //llamar los archivos javascript con la primera letra en minúscula y el nombre de las imágenes también
-for (var a=[],i=1;i<37;++i) a[i]=i;
 
-// http://stackoverflow.com/questions/962802#962890
-function shuffle(array) {
-  var tmp, current, top = array.length;
-  if(top) while(--top) {
-    current = Math.floor(Math.random() * (top + 1));
-    tmp = array[current];
-    array[current] = array[top];
-    array[top] = tmp;
-  }
-  return array;
+//Generar un random
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+//almaceno todos los random obtenidos en un array
+var random = [];
+for(var i=0; i<36; i++){
+	random[i]=randomIntFromInterval(1,28);
 }
 
-a = shuffle(a);
 for(var i=1; i<37; i++){
-	document.getElementById("backImg" + i).style.backgroundImage = 'url(imagenes/' + a[i-1] + '.png)';
-}
-for(var i=28; i<37; i++){
-	document.getElementById("backImg" + i).style.backgroundImage = 'url(imagenes/' + a[i-1] + '.png)';
+	document.getElementById("backImg" + i).style.backgroundImage = 'url(imagenes/' + random[i-parseInt(1)] + '.png)';
 }
