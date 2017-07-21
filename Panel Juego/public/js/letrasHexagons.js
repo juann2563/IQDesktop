336a9da4fed8509f73e311dbf205170f315169fc
juann2563 <juanzabala2193@gmail.com>
2017-07-20 19:37:07 -0500

    pruebas

--- Panel Juego/public/js/letrasHexagons.js
+++ Panel Juego/public/js/letrasHexagons.js
@@ -1,4 +1,22 @@
 //llamar los archivos javascript con la primera letra en minúscula y el nombre de las imágenes también
+for (var a=[],i=1;i<37;++i) a[i]=i;
+
+// http://stackoverflow.com/questions/962802#962890
+function shuffle(array) {
+  var tmp, current, top = array.length;
+  if(top) while(--top) {
+    current = Math.floor(Math.random() * (top + 1));
+    tmp = array[current];
+    array[current] = array[top];
+    array[top] = tmp;
+  }
+  return array;
+}
+
+a = shuffle(a);
 for(var i=1; i<37; i++){
-	document.getElementById("backImg" + i).style.backgroundImage = 'url(imagenes/a.png)';
+	document.getElementById("backImg" + i).style.backgroundImage = 'url(imagenes/' + a[i-1] + '.png)';
+}
+for(var i=28; i<37; i++){
+	document.getElementById("backImg" + i).style.backgroundImage = 'url(imagenes/' + a[i-1] + '.png)';
 }
