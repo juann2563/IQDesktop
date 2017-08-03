1b5d10bc133dd45014ca708c46d9fb2329d847e6
juann2563 <juanzabala2193@gmail.com>
2017-08-03 16:36:14 -0500

    added node modules

--- Panel_Juego_electron/main.js
+++ Panel_Juego_electron/main.js
@@ -9,7 +9,7 @@ let win
 function createWindow(){
 	win = new BrowserWindow ({width: 800, height:600})
 	win.loadURL(url.format({
-		pathname: path.join(__dirname, 'index.html'),
+		pathname: path.join(__dirname, 'serialportPrueba.html'),
 		protocol: 'file',
 		slashes: true
 	}))
