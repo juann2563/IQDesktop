ee456516d885a9c57d1191cf420202b34fa6b114
juann2563 <juanzabala2193@gmail.com>
2017-08-04 10:50:49 -0500

    fixed preloader

--- Panel_Juego_electron/main.js
+++ Panel_Juego_electron/main.js
@@ -9,7 +9,7 @@ let win
 function createWindow(){
 	win = new BrowserWindow ({width: 800, height:600})
 	win.loadURL(url.format({
-		pathname: path.join(__dirname, 'serialportPrueba.html'),
+		pathname: path.join(__dirname, 'index.html'),
 		protocol: 'file',
 		slashes: true
 	}))
