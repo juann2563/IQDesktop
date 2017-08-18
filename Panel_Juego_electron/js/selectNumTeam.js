7357d2ff73fef933ea8aaee7f5ede5ba59d9878b
juann2563 <juanzabala2193@gmail.com>
2017-08-18 08:49:49 -0500

    Fixed SelectNumTeam, there was a bug when added number in BD, Added two num and it needed only one

--- Panel_Juego_electron/js/selectNumTeam.js
+++ Panel_Juego_electron/js/selectNumTeam.js
@@ -1,3 +1,5 @@
+
+//Add to database the number of team to participated
 function insertNumTeam(){
 	var numteam = $('.selectTeam').text();
 	var sqlite3 = require('sqlite3').verbose();
@@ -15,7 +17,7 @@ function insertNumTeam(){
 	var statement = db.prepare("INSERT INTO 'configuracion' ('num_equipos') " +
 	'VALUES (?)');
 	//ejecuto el query a la base de datos
-	statement.run("comer");
+	statement.run(numteam);
 	statement.finalize();
 	// Close the database:
 	
@@ -24,12 +26,14 @@ function insertNumTeam(){
 	
 	$('.myModal').fadeOut(1000);
 }
+
+// determinate what is the selected option for number of teams
 function numTeam(clicked_id){
 	var x = document.getElementById(clicked_id);
 	x.classList.add("selectTeam");
 	for(var i=1; i<4; i++){
 				
-				if("btn"+i == id){
+				if("btn"+i == clicked_id){
 				}
 				else {
 					$('#btn'+i).removeClass('selectTeam');
