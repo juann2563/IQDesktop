ca104e7846dfb755b696aa8c4edc326667b10f5b
juann2563 <juanzabala2193@gmail.com>
2017-08-18 09:50:13 -0500

    fixed bugs in SelectNumTeam for reset default setting when the user does not click button in other options. Added other field in panel Juego modal window

--- Panel_Juego_electron/js/selectNumTeam.js
+++ Panel_Juego_electron/js/selectNumTeam.js
@@ -1,7 +1,16 @@
-
+//Variable para manener un default de equipos por si el usuario
+// no selecciona ninguna opción
+var state = 0;
 //Add to database the number of team to participated
 function insertNumTeam(){
-	var numteam = $('.selectTeam').text();
+	if(state == 0){
+		var btn = document.getElementById("btn1");
+		btn.classList.add("selectTeam");	
+	}
+	else{
+		var numteam = $('.selectTeam').text();	
+	}
+	
 	var sqlite3 = require('sqlite3').verbose();
 	//var fs  = require('fs');
 	//const path = require('path');
@@ -29,11 +38,14 @@ function insertNumTeam(){
 
 // determinate what is the selected option for number of teams
 function numTeam(clicked_id){
+	// state=1 because  this way when it insert data into DB determine id insert default teams of change
+	state = 1;
 	var x = document.getElementById(clicked_id);
 	x.classList.add("selectTeam");
 	for(var i=1; i<4; i++){
 				
 				if("btn"+i == clicked_id){
+					
 				}
 				else {
 					$('#btn'+i).removeClass('selectTeam');
