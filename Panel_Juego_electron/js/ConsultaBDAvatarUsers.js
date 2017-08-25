09ab844287114c444af5a546a9da2e1a62d471f4
juann2563 <juanzabala2193@gmail.com>
2017-08-25 14:13:59 -0500

    some changes

--- Panel_Juego_electron/js/ConsultaBDAvatarUsers.js
+++ Panel_Juego_electron/js/ConsultaBDAvatarUsers.js
@@ -11,12 +11,15 @@ function avatarUsers(equipo){
 
     // Initialize the database:
     var db = new sqlite3.Database(dbFile);
-    db.all("SELECT * FROM usuarios ORDER BY ID DESC LIMIT 4", function(err, rows) {
-        $('#imgEquipo'+ equipo).attr('src', ''+ rows[parseInt(equipo)-1].avatar);
+    db.serialize(function (){
+        db.all("SELECT * FROM usuarios ORDER BY ID DESC LIMIT 4", function(err, rows) {
+        $('#imgEquipo'+ equipo).attr('src', './'+ rows[parseInt(equipo)-1].avatar);
         $('#textEquipo' + equipo).text('' + rows[parseInt(equipo)-1].equipo);
         $('#colorEquipo' + equipo).css('background-color','' + rows[parseInt(equipo)-1].color);
         $('#modaltextEquipo4').html('' + rows[parseInt(equipo)-1].equipo);
+        });
     });
+    
       
     db.close();
 }
