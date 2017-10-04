bb383262fc7e22d3e0d3402e9d2a303ea8d21839
juann2563 <juanzabala2193@gmail.com>
2017-10-04 08:01:04 -0500

    Added same class for imgEquipo
    Added same class for imgEquipo for change properties to all elements by the same class

--- Panel_Juego_electron/js/ConsultaBDAvatarUsers.js
+++ Panel_Juego_electron/js/ConsultaBDAvatarUsers.js
@@ -13,10 +13,26 @@ function avatarUsers(equipo){
     var db = new sqlite3.Database(dbFile);
     db.serialize(function (){
         db.all("SELECT * FROM usuarios ORDER BY ID DESC LIMIT 4", function(err, rows) {
-        $('#imgEquipo'+ equipo).attr('src', './'+ rows[parseInt(equipo)-1].avatar);
-        $('#textEquipo' + equipo).text('' + rows[parseInt(equipo)-1].equipo);
-        $('#colorEquipo' + equipo).css('background-color','' + rows[parseInt(equipo)-1].color);
-        $('#modaltextEquipo4').html('' + rows[parseInt(equipo)-1].equipo);
+            //var img = $(".imgEquipo" + equipo);
+            //alert(rows[0].avatar);
+            //alert(img.length);
+            //alert(imgLength.length);
+            // se obtiene la cantidad de elements que existen por la misma clase
+            var imagenes = document.getElementsByClassName("imgEquipo" + equipo);
+            
+                
+            for(var j = 0; j<imagenes.length; j++){}
+                // se le asigna la msima imagen a todos los elementos con la msima clase
+                imagenes[j].src = './'+ rows[parseInt(equipo)-1].avatar; // cambia la imagen a todas la imagenes con la misma clase  
+            }
+                
+                               // $(".imgEquipo" + parseInt(i+1)).attr('src', './'+ rows[parseInt(equipo)-1].avatar);    
+            
+            // Se le asigna el nombre al equipo correspondiente
+            $('#textEquipo' + equipo).text('' + rows[parseInt(equipo)-1].equipo);
+            // Se le asigna el color a cada equipo
+            $('#colorEquipo' + equipo).css('background-color','' + rows[parseInt(equipo)-1].color);
+            $('#modaltextEquipo4').html('' + rows[parseInt(equipo)-1].equipo);
         });
     });
     
