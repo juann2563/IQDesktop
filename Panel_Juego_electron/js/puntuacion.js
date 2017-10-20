88967fe53b350bf69da000ebb77639e5ba18178e
juann2563 <juanzabala2193@gmail.com>
2017-10-20 14:08:04 -0500

    probando nuevas caracteristica
    Probando cambiar background de los hexagonos

--- Panel_Juego_electron/js/puntuacion.js
+++ Panel_Juego_electron/js/puntuacion.js
@@ -23,6 +23,7 @@ function puntuacionCorrecto(equipo)
 		//var x = document.getElementById("Equipo" + equipo + "Correcto");
 	//aalert(alert($("#Equipo" + equipo + "Correcto").css('width')););
 	//var px = $("#Equipo" + equipo + "Correcto").css('width');
+	//Se obtiene el nmbre del equipo al cual se le hizo clic
 		var nameEquipo = $("#textEquipo" + equipo).text();
 		//alert(nameEquipo);
 		var px = $("#Equipo" + equipo + "Correcto").width();
@@ -42,15 +43,32 @@ function puntuacionCorrecto(equipo)
 
 	    var db = new sqlite3.Database(dbFile);
 	    //var query1 = nameEquipo;
-	    var query = "SELECT color FROM usuarios WHERE equipo = 'zabala'";
+	    var query = "SELECT color FROM usuarios WHERE equipo = 'zabala'"; // pendiente por verifficar por qué no funciona
 	    // consulta la base de datos
 	    db.serialize(function(){
-	        db.all("SELECT color FROM usuarios WHERE equipo = " + "'" + nameEquipo + "'", function(err, rows){
-	            //Consulta la base de datos para obtener el color del equipo y asignarlo a la imagen
-	            var imgChange = $("#imgActual").text();
-	            alert(query);
-	            alert(rows);
-	            //$("#"+ imgChange).css('background-color',''+rows[0].color);  
+	        db.all("SELECT *FROM usuarios", function(err, rows){
+	        	//alert(typeof nameEquipo);
+	        	//alert(nameEquipo + rows[0].color);
+	        	for(var i=0; i<rows.length; i++)
+	        	{
+	        		//alert(rows[i].equipo + "" + nameEquipo);
+	        		
+	        		if(rows[i].equipo.trim() == nameEquipo.trim()) // el .trim() elimina los espacio que hayan en el string
+	        		{
+	        			//alert("Hoal");
+	        			var imgChange = $("#imgActual").text();
+	        			//var parentId = $("#"+imgChange).closest('div').prop('id');
+    					//alert(parentId);
+	        			//var imgChange = $("#imgActual").text().match(/\d/g).join("");
+	        			//alert(x);
+	        			$("#backImg" + imgChange).css("background", "none");
+	        			$("#backImg" + ).css("background", "none");
+	        			//document.getElementById("backImg" + imgChange).style.backgroundColor = "" + rows[i].color;
+	        			alert("ImagenCambiada: " + rows[i].color);
+	        			break;	
+
+	        		}
+	        	} 
 	        });
 	   
 	    });
