d5970258b49add588a13006ee86a41ad334abeab
juann2563 <juanzabala2193@gmail.com>
2017-11-05 13:58:53 -0500

    Added change letra withiut unout value

--- Panel_Juego_electron/js/cambiarLetra.js
+++ Panel_Juego_electron/js/cambiarLetra.js
@@ -4,6 +4,11 @@ function CambiarLetra ()
 {
 	var letraCambiar = $('#myModal #ChangeLetra').val().trim().toUpperCase().charCodeAt();
 	//alert(letraCambiar);
+	//alert(letraCambiar);
+	if(isNaN(letraCambiar)){
+		letraCambiar = "A";
+		letraCambiar = letraCambiar.charCodeAt();
+	}
 	var idImage = $("#imgActual").text();
 	document.getElementById('' + idImage).style.backgroundImage = "url(imagenes/" + letraCambiar +".png)";
 }
